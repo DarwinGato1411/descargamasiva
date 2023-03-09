d501eefc6c933483443756560ac4c2fba8e6f44d
Darwin <darwinvinicio14_11@hotmail.com>
2023-03-09 11:45:16 -0500

    Darwin

---
+++ Masiva/build/generated-sources/ap-source-output/com/ec/entidad/Costopromediocompraview2_.java
@@ -0,0 +1,19 @@
+package com.ec.entidad;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-03-09T11:22:36")
+@StaticMetamodel(Costopromediocompraview2.class)
+public class Costopromediocompraview2_ { 
+
+    public static volatile SingularAttribute<Costopromediocompraview2, String> descripcion;
+    public static volatile SingularAttribute<Costopromediocompraview2, Date> fecha;
+    public static volatile SingularAttribute<Costopromediocompraview2, BigDecimal> subtotal;
+    public static volatile SingularAttribute<Costopromediocompraview2, BigInteger> id;
+
+}
---
+++ Masiva/build/generated-sources/ap-source-output/com/ec/entidad/TotalizadoRubros_.java
@@ -0,0 +1,22 @@
+package com.ec.entidad;
+
+import java.math.BigDecimal;
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2023-03-09T11:22:36")
+@StaticMetamodel(TotalizadoRubros.class)
+public class TotalizadoRubros_ { 
+
+    public static volatile SingularAttribute<TotalizadoRubros, BigDecimal> total;
+    public static volatile SingularAttribute<TotalizadoRubros, BigDecimal> subtotal;
+    public static volatile SingularAttribute<TotalizadoRubros, BigDecimal> total12;
+    public static volatile SingularAttribute<TotalizadoRubros, BigDecimal> total0;
+    public static volatile SingularAttribute<TotalizadoRubros, Integer> codTipoambiente;
+    public static volatile SingularAttribute<TotalizadoRubros, Integer> id;
+    public static volatile SingularAttribute<TotalizadoRubros, String> clasificacion;
+    public static volatile SingularAttribute<TotalizadoRubros, Date> cabFecha;
+
+}
